95f2cf74d7ea24e2b4bc696cd82d54f907dcb2d4
dhanraj shetty <dhanrajshetty1997@gmail.com>
2022-03-28 12:29:42 +0530

    added accordian functionality

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-$(document).ready(function(){
-    
-})
-
+$(document).ready(function () {
+    $('.accordian-item').click(function () {
+        $(this).toggleClass('active');
+        $('div.panel').slideUp();
+        if ($(this).hasClass('active')) {
+            $(this).siblings().removeClass('active');
+            $(this).find('div.panel').slideDown();
+        } else {
+            $(this).find('div.panel').slideUp()
+        }
+    });
+});
